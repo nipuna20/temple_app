335aaf46a96b5888f7ef638c3d5ef452e3a61577
nipuna20 <nipunau739@gmail.com>
2026-03-18 01:14:51 +0530

    sixth

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,65 @@
-const path = require('path');
+// const path = require('path');
+
+// module.exports = {
+//   entry: './src/index.js',
+//   output: {
+//     path: path.resolve(__dirname, 'dist'),
+//     filename: 'bundle.js',
+//     publicPath: '/',
+//     clean: true,
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.jsx?$/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: 'babel-loader',
+//           options: {
+//             presets: ['@babel/preset-env', '@babel/preset-react'],
+//           },
+//         },
+//       },
+//       {
+//         test: /\.css$/,
+//         use: ['style-loader', 'css-loader'],
+//       },
+//       {
+//         test: /\.(png|jpg|jpeg|gif|svg)$/,
+//         type: 'asset',
+//       },
+//     ],
+//   },
+//   resolve: {
+//     extensions: ['.js', '.jsx'],
+//   },
+//   devServer: {
+//     static: path.join(__dirname, 'public'),
+//     historyApiFallback: true,
+//     port: 3001,
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:4000',
+//         changeOrigin: true,
+//       },
+//     },
+//   },
+// };
+
+
+
+
+
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/',
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/",
     clean: true,
   },
   module: {
@@ -14,32 +68,37 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
+            presets: ["@babel/preset-env", "@babel/preset-react"],
           },
         },
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|jpg|jpeg|gif|svg)$/,
-        type: 'asset',
+        type: "asset",
       },
     ],
   },
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: [".js", ".jsx"],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public/index.html"),
+    }),
+  ],
   devServer: {
-    static: path.join(__dirname, 'public'),
+    static: path.join(__dirname, "public"),
     historyApiFallback: true,
-    port: 3001,
+    port: 3000,
     proxy: {
-      '/api': {
-        target: 'http://localhost:4000',
+      "/api": {
+        target: "http://localhost:4000",
         changeOrigin: true,
       },
     },
